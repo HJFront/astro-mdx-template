7d24b699d39afbb423884a7d7b718e1623a4d37f
WuChenDi <wuchendi96@gmail.com>
2022-08-20 19:47:07 +0800

    feat: add prefix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
+export const BASE_URL = import.meta.env.BASE_URL;
 export const SITE_TITLE = 'astro-mdx-template';
 export const SITE_DESCRIPTION = '🎉 using astro with mdx to generate page modules';
