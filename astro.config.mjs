84a04fdbfc931ce7cfd4346681a869ca33bd95ee
WuChenDi <wuchendi96@gmail.com>
2022-08-21 09:08:23 +0800

    feat: Modify configuration items site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/HJFront',
+  site: 'https://github.com',
   base: '/astro-mdx-template',
 	integrations: [mdx(), react(), sitemap()],
 });
